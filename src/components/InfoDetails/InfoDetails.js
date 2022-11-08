0043dc7c20877064d98f9392715d85b52613c421
opuzayed <zayedopu4933@gmail.com>
2022-11-08 23:06:04 +0600

    personal information added

--- src/components/InfoDetails/InfoDetails.js
+++ src/components/InfoDetails/InfoDetails.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import img from '../../Assets/Images/opu.jpg';
+import './InfoDetails.css';
 const InfoDetails = () => {
     return (
-        <div>
-           <h1>Information details here</h1> 
+        <div style={{marginLeft:'10px', marginTop:'20px'}}>
+            <div className='img-style'>
+                <img src={img} alt="" />
+                <h4>Zayed Hossain</h4>
+            </div>
+            <p style={{textAlign:'justify'}}>
+                I help skinny people to build muscle without lifting weights.
+                I help people like you to lose weight without breaking a sweat.
+                We help athletes to up their game through proper nutrition.
+            </p>
         </div>
     );
 };
