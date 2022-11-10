67ea0aa7cea35b2170f3d552ea9a736f0fc57c47
opuzayed <zayedopu4933@gmail.com>
2022-11-11 00:02:57 +0600

    border added in info-details component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -16,21 +16,7 @@ const Main = () => {
   }, []);
 
   const handleAddTime = (selectedProduct) => {
-    //console.log(selectedProduct);
-    /* let newCart = [];
-        const exists = cart.find(product => product.id === selectedProduct.id);
-        if(!exists){
-            selectedProduct.quantity = 1;
-            newCart = [...cart, selectedProduct];
-        }
-        else{
-            const rest = cart.filter(product => product.id !== selectedProduct.id);
-            exists.quantity = exists.quantity + 1;
-            newCart = [...rest, exists];
-        }
-        
-        setCart(newCart); */
-    // addToDb(selectedProduct.id);
+    
   };
 
   return (
